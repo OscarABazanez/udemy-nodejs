22ae88f6951b7da6a6e0afcebf4c9198a2c6e313
OscarABazanez <oscar-bazanez@hotmail.com>
2021-07-29 15:24:28 -0500

    Creaando el gitignore

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,6 +1,6 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 
-const base = 52
+const base = 532
 
 crearArchivo(base)
     .then( nombreArchivo => console.log(nombreArchivo, 'Creado'))
