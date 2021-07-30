46d06b448bf00b2015eb3644510aef019dd99202
OscarABazanez <oscar-bazanez@hotmail.com>
2021-07-30 10:04:18 -0500

    Instalando y usando yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,15 @@
 const { crearArchivo } = require('./helpers/multiplicar');
+// node app.js --base=5 --base2=111 En terminal
+const argv = require('yargs').argv;
+console.log('base yargs: ',argv.base)
+console.log('base yargs: ',argv.base2)
+
 
 const base = 532
 
-crearArchivo(base)
-    .then( nombreArchivo => console.log(nombreArchivo, 'Creado'))
-    .catch( err => console.log(err))
+// crearArchivo(base)
+//     .then( nombreArchivo => console.log(nombreArchivo, 'Creado'))
+//     .catch( err => console.log(err))
+
+
 
