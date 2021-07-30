2cfc32a930a68195149780e1e9d3ba506944cc37
OscarABazanez <oscar-bazanez@hotmail.com>
2021-07-30 10:24:20 -0500

    Configurando yargs

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,15 +1,36 @@
 const { crearArchivo } = require('./helpers/multiplicar');
 // node app.js --base=5 --base2=111 En terminal
-const argv = require('yargs').argv;
-console.log('base yargs: ',argv.base)
-console.log('base yargs: ',argv.base2)
+const argv = require('yargs')
+    .option('b', {
+        alias: 'base',
+        type: 'number',
+        demandOption: true,
+    })
+    .option('l', {
+        alias: 'listar',
+        type: 'boolean',
+        demandOption: false,
+        default: false
+    })
+    .check((argv, options) => {
+        if( isNaN(argv.b)){
+            throw 'La base tiene que ser un numero'
+        }
+        return true;
+    })
+    .argv;
+
+const base = argv.b
+const lista = argv.l
+console.log('base yargs: ',base)
 
 
-const base = 532
 
-// crearArchivo(base)
-//     .then( nombreArchivo => console.log(nombreArchivo, 'Creado'))
-//     .catch( err => console.log(err))
+// const base = 5324
+
+crearArchivo(base,lista)
+    .then( nombreArchivo => console.log(nombreArchivo, 'Creado'))
+    .catch( err => console.log(err))
 
 
 
--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,14 +1,18 @@
 const fs = require('fs');
 
 
-const crearArchivo = async (base = 5) =>{
+const crearArchivo = async (base = 5, listar = false) =>{
     try {
         let salida = `Tabla del ${base} \n`;
 
         for (let index = 1; index <= 10; index++) {
             salida += `${base} x ${index} = ${base*index}\n`;
         }
-    
+
+        if(listar){
+            console.log(salida)
+        }
+        
         fs.writeFile(`tabla-del-${base}.txt`, salida, (err) =>{
             if(err) throw err;
             console.log('tabla creada');
